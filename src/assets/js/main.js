be218ef5cb9415851354fc522a9dcc1b20f21771
xanderyanov <xanderyanov@gmail.com>
2021-11-26 15:37:52 +0400

    321321321

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -344,24 +344,12 @@ var a = [
 $(function () {});
 
 $("#myButton").on("click", function () {
-	console.log("btn click");
 	focusIn();
-});
-// $("#myButton").on("focus", function () {
-// 	focusIn();
-// });
-
-//????
-$("myInput").on("click", function () {
-	if (this.val() == "") {
-		focusIn();
-	}
 });
 
 $(document).on("mouseup", function (e) {
 	var container = $(".block");
 	if (container.has(e.target).length === 0) {
-		// container.hide();
 		focusOut();
 	}
 });
@@ -370,32 +358,24 @@ function focusOut() {
 	$("#myButton").show().focus();
 	$("#myInput").hide();
 	$("#list").slideUp(200);
-	// $(".list").removeClass("selected");
 }
 
 function focusIn() {
 	setTimeout(() => {
 		$("#myButton").hide();
 		$("#myInput").show().focus().select();
-		$("#list").slideDown(200);
+		$("#list").slideDown(200).scrollTop("0");
 		listNavigate();
 	}, 100);
 }
 
-// function textInButton() {
-// 	var t = $("#myInput").val();
-// 	$("#myButton").text(t);
-// 	if (t == "") $("#myButton").text("Введите значние");
-// }
-
 //Формируем список в выпадайке
 var b = [];
 function list() {
-	// var tabindexVal = parseInt($("#myInput").attr("tabindex"));
 	$.each(a, function (i) {
 		var id = a[i].id;
-		var lwrList = a[i].cityName.toLowerCase(); // массив в нижний регистр - для поиска
-		var uppList = lwrList.charAt(0).toUpperCase() + lwrList.substr(1); //Названия делаем с большой буквы
+		var lwrList = a[i].cityName.toLowerCase();
+		var uppList = lwrList.charAt(0).toUpperCase() + lwrList.substr(1);
 		b[i] =
 			'<div class="list" data-title="' +
 			uppList +
@@ -417,64 +397,70 @@ function listNavigate() {
 	var liSelected;
 	$("#myInput").on("keyup", function (e) {
 		let listcont = $("#list");
+		let allVisible = $("#list").children(".list:visible");
+		let currentIndex = allVisible.index(liSelected);
 		if (e.key == "ArrowDown") {
 			if (liSelected) {
-				let allVisible = $("#list").children(".list:visible");
-				let currentIndex = allVisible.index(liSelected);
-				if (currentIndex >= allVisible.length - 1) return;
-				liSelected.removeClass("selected");
-				next = allVisible.eq(currentIndex + 1); // liSelected.next();
-				// let listcont = $("#list");
-				if (next.length > 0) {
-					liSelected = next.addClass("selected");
+				// let allVisible = $("#list").children(".list:visible");
+				// let currentIndex = allVisible.index(liSelected);
 
-					listcont.scrollTop(listcont.scrollTop() + next.position().top);
-				} else {
+				liSelected.removeClass("selected");
+				next = allVisible.eq(currentIndex + 1);
+				if (currentIndex >= allVisible.length - 1) {
+					console.log("возвращаемся снизу наверх");
 					liSelected = li.eq(0).addClass("selected");
 					listcont.scrollTop(listcont.scrollTop() + liSelected.position().top);
+					return;
+				}
+				if (next.length > 0) {
+					liSelected = next.addClass("selected");
+					listcont.scrollTop(listcont.scrollTop() + next.position().top);
+					console.log("идем вниз");
 				}
 			} else {
 				liSelected = li.eq(0).addClass("selected");
+				console.log("первый раз вниз");
 			}
+			$("#myInput").val(liSelected.data("title"));
 		} else if (e.key == "ArrowUp") {
 			if (liSelected) {
-				let allVisible = $("#list").children(".list:visible");
-				let currentIndex = allVisible.index(liSelected);
-				if (currentIndex <= 0) return;
+				// let allVisible = $("#list").children(".list:visible");
+				// let currentIndex = allVisible.index(liSelected);
 				liSelected.removeClass("selected");
-				prev = allVisible.eq(currentIndex - 1); // liSelected.next();
-				// let listcont = $("#list");
+				prev = allVisible.eq(currentIndex - 1);
+				if (currentIndex <= 0) {
+					console.log("возвращаемся сверху вниз");
+					liSelected = li.eq(currentIndex).removeClass("selected");
+					liSelected = li.last().addClass("selected");
+					listcont.scrollTop(listcont.scrollTop() + liSelected.position().top);
+					return;
+				}
 				if (prev.length > 0) {
 					liSelected = prev.addClass("selected");
 					listcont.scrollTop(listcont.scrollTop() + prev.position().top);
-				} else {
-					liSelected = li.last().addClass("selected");
-					listcont.scrollTop(listcont.scrollTop() + liSelected.position().top);
+					console.log("идем вверх");
 				}
 			} else {
 				liSelected = li.last().addClass("selected");
 				listcont.scrollTop(listcont.scrollTop() + liSelected.position().top);
+				console.log("первый раз стрелкой вверх попадаем в конец списка");
 			}
+			$("#myInput").val(liSelected.data("title"));
 		}
+
 		if (e.key == "Enter") {
 			console.log("Enter");
 			if (liSelected) {
-				// var val = liSelected.data("title");
-				// $("#myInput").val(val);
-				// $("#list").slideUp(200);
-				// textInButton();
-				// focusOut();
-				// // liSelected = null;
 				newCheck(liSelected);
+				liSelected.removeClass("selected");
 				liSelected = null;
+				$("#list").scrollTop("0");
 			}
 		}
 	});
 }
-
+//работает этот
 function newCheck(event) {
-	// $("#myInput").val($(event).data("title"));
-	// console.log($(event).data("title"));
 	$("#myInput").val($(event).data("title"));
 	$("#list").slideUp(200);
 	focusOut();
@@ -485,10 +471,7 @@ function newCheck(event) {
 checking();
 function checking() {
 	$(".list").on("click", function () {
-		// $(".list").not(this).removeClass("selected");
-		// $(this).addClass("selected");
 		$("#myInput").val($(this).data("title"));
-
 		textInButton();
 		focusOut();
 	});
@@ -500,7 +483,6 @@ $("#myInput").on("input", function () {
 });
 
 function search() {
-	$("#list").slideDown(200);
 	setTimeout(function () {
 		var lwrSrch = $("#myInput").val().toLowerCase();
 		let match = $("#list").children();
@@ -510,7 +492,7 @@ function search() {
 				if (d.is('[data-id*="' + lwrSrch + '"]')) d.show();
 				else d.hide(); /*.attr("tabindex", "0")*/
 			});
-			checking();
+			// newCheck(d);
 		}
 	}, 50);
 }
